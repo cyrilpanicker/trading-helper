90b2a93a9cd991ef98f1cbf4d82ddf1cc48a2dfc
Cyril Panicker <cyril.panicker@ibm.com>
2022-09-25 21:47:56 +0530

    refactored

--- server/placeOrder.js
+++ server/placeOrder.js
@@ -1,24 +1,22 @@
 
-const getCurrentPrice = async (kc, addNote, res, exchange, tradingsymbol) => {
+const getCurrentPrice = async (kc, addNote, exchange, tradingsymbol) => {
     const tradingsymbolWithExchange = `${exchange}:${tradingsymbol}`;
     let quoteResponse = null;
     try {
         quoteResponse = await kc.getQuote(tradingsymbolWithExchange);
     } catch (error) {
         addNote('error occurred while fetching current price', error)
-        res.status(500).send({ error: error ? error.message : 'error occurred while fetching current price' })
-        return
+        return null
     }
     if (!quoteResponse[tradingsymbolWithExchange] || !quoteResponse[tradingsymbolWithExchange].last_price) {
         addNote('error occurred while fetching current price')
-        res.status(500).send({ error: 'error occurred while fetching current price' })
-        return
+        return null
     }
     addNote('current price fetched successfully', null, true)
     return quoteResponse[tradingsymbolWithExchange].last_price;
 }
 
-const placeOrderFunctionCreator = (kc, addNote, res, exchange, tradingsymbol, quantity) => {
+const placeOrderFunctionCreator = (kc, addNote, exchange, tradingsymbol, quantity) => {
     return async (orderName, transaction_type, order_type, price, trigger_price) => {
         const orderParams = {
             exchange, tradingsymbol, transaction_type,
@@ -36,7 +34,6 @@ const placeOrderFunctionCreator = (kc, addNote, res, exchange, tradingsymbol, qu
             orderResponse = await kc.placeOrder('regular', orderParams)
         } catch (error) {
             addNote(`error occurred while placing ${orderName} order`, error)
-            res.status(500).send({ error: error ? error.message : `error occurred while placing ${orderName} order` })
             return false
         }
         addNote(`${orderName} order placed`, orderResponse, true)
@@ -47,19 +44,28 @@ const placeOrderFunctionCreator = (kc, addNote, res, exchange, tradingsymbol, qu
 module.exports = function (kc, addNote) {
     return async (req, res) => {
         const { exchange, tradingsymbol, transaction_type, higherPrice, lowerPrice, maxLoss } = req.body;
-        const currentPrice = await getCurrentPrice(kc, addNote, res, exchange, tradingsymbol);
-        if (!currentPrice) return
+        const currentPrice = await getCurrentPrice(kc, addNote, exchange, tradingsymbol);
+        if (!currentPrice) {
+            res.status(500).send()
+            return
+        }
         const takeProfitPrice = transaction_type === 'BUY' ? higherPrice : lowerPrice;
         const stopLossPrice = transaction_type === 'BUY' ? lowerPrice : higherPrice;
         const quantity = Math.floor(maxLoss / Math.abs(currentPrice - stopLossPrice));
         const exitTransactionType = transaction_type === 'BUY' ? 'SELL' : 'BUY';
-        const placeOrder = placeOrderFunctionCreator(kc, addNote, res, exchange, tradingsymbol, quantity);
+        const placeOrder = placeOrderFunctionCreator(kc, addNote, exchange, tradingsymbol, quantity);
         const entryOrderPlaced = await placeOrder('entry-order', transaction_type, 'MARKET', null, null)
-        if (!entryOrderPlaced) return
-        const stopLossOrderPlaced = await placeOrder('stop-loss', exitTransactionType, 'SL-M', null, stopLossPrice)
-        if (!stopLossOrderPlaced) return
-        const takeProfitOrderPlaced = await placeOrder('take-profit', exitTransactionType, 'LIMIT', takeProfitPrice, null)
-        if (!takeProfitOrderPlaced) return
+        if (!entryOrderPlaced) {
+            res.status(500).send()
+            return
+        }
+        const stopLossOrderPlaced = placeOrder('stop-loss', exitTransactionType, 'SL-M', null, stopLossPrice)
+        const takeProfitOrderPlaced = placeOrder('take-profit', exitTransactionType, 'LIMIT', takeProfitPrice, null)
+        const exitOrdersPlaced = await Promise.all([stopLossOrderPlaced, takeProfitOrderPlaced]);
+        if (!exitOrdersPlaced[0] || !exitOrdersPlaced[1]) {
+            res.status(500).send()
+            return
+        }
         addNote(`order placed: ${transaction_type} - ${tradingsymbol} - ${quantity} - ${currentPrice} - ${takeProfitPrice} - ${stopLossPrice}`)
         res.send({ data: 'order placed' })
 
